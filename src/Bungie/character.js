f7f84edf1287fb53a95c2f1a8c34101b43927d8e
Phillip Jackson <unisys12@gmail.com>
2017-01-21 22:25:20 -0600

    corrected pattern

--- src/Bungie/character.js
+++ src/Bungie/character.js
@@ -4,44 +4,67 @@ const Bungie = require("./api")
 
 let Character = (function() {
 
+    let getCharacterId = (membershipType, displayName) => {
+
+        return new Promise((resolve, reject) => {
+
+            Bungie.getIdByDisplayName(membershipType, displayName)
+
+                .then((response) => {
+                    resolve(response)
+                })
+
+                .catch((error) => {
+                    reject(error)
+                })
+
+        })
+
+    }
+
+    let getCharacterSummary = (membershipType, membershipId) => {
+
+        return new Promise((resolve, reject) => {
+
+            Bungie.getSummary(membershipType, membershipId)
+
+                .then((response) => {
+                    resolve(response)
+                })
+
+                .catch((error) => {
+                    reject(error)
+                })
+
+        })
+
+    }
+
+    let getSummary = (membershipType, membershipId) => {
+
+        getCharacterSummary(membershipType, membershipId)
+        .then((response) => {
+            return response
+        }, (err) => {
+            return err
+        })
+
+    }
+
+    let getId = (membershipType, displayName) => {
+
+        getCharacterId(membershipType, displayName)
+        .then((response) => {
+            return response
+        }, (err) => {
+            return err
+        })
+
+    }
+
     return {
-
-        getCharacterId: (membershipType, displayName) => {
-
-            new Promise((resolve, reject) => {
-
-                Bungie.getIdByDisplayName(membershipType, displayName)
-
-                .then((response) => {
-                    resolve(response)
-                })
-
-                .catch((error) => {
-                    reject(error)
-                })
-
-            })
-
-        },
-
-        getCharacterSummary: (membershipType, membershipId) => {
-
-            new Promise((resolve, reject) => {
-
-                Bungie.getSummary(membershipType, membershipId)
-
-                .then((response) => {
-                    resolve(response)
-                })
-
-                .catch((error) => {
-                    reject(error)
-                })
-
-            })
-
-        }
-
+        getSummary: getSummary,
+        getId: getId
     }
 
 })()
