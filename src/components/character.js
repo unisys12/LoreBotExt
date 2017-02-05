d13e94f03491b9869e45379afe97da6c21daa292
Phillip Jackson <unisys12@gmail.com>
2017-02-05 10:35:39 -0600

    use co as mention in pr #2

--- src/components/character.js
+++ src/components/character.js
@@ -1,11 +1,11 @@
 'use strict'
 
 import Vue from 'vue'
-let VueResource = require('vue-resource')
+import { getIdByDisplayName, getSummary } from '../Bungie/api.js'
+let co = require('co')
 
-Vue.use(VueResource)
 
-let character = new Vue({
+new Vue({
 
     el: '#character',
 
@@ -13,75 +13,62 @@ let character = new Vue({
         message: 'Waiting for your characters to enter an activity...',
         displayName: '',
         membershipType: '',
+        membershipId: '',
         characterBase: [],
         activeCharacter: ''
     },
 
-    // Configure Vue-Resource Options
-    http: {
-        headers: {
-            'X-API-KEY': '7ced29b9f06844efb9102fbf73218362'
-        }
-    },
-
     methods: {
-        assembleCharacters: function() {
-          console.log('Assembling Characters')
-          let vm = this.$data;
+        assembleCharacters: co.wrap(function *() {
+          
+          let vm = this
+          
+          try {
+          
+          let response = yield getIdByDisplayName(vm.membershipType, vm.displayName)
 
-          this.$http.get('http://www.bungie.net/Platform/Destiny/' + vm.membershipType + '/Stats/GetMembershipIdByDisplayName/' + vm.displayName + '/')
-          .then(function (response) {
+          console.log('ID: ' + response.data.Response)
 
-              vm.membershipId = response.body.Response;
-              return this.$http.get('http://www.bungie.net/Platform/Destiny/' + vm.membershipType + '/Account/' + vm.membershipId + '/Summary/')
+          vm.membershipId = response.data.Response;
+          
+          if (vm.membershipId === '') {
 
-          }, function (err) {
-              vm.message = 'Failed to fetch your MembershipID: ' + err;
-          })
-          .then(function (response) {
+              vm.message = 'Failed to fetch your Bungie Membership ID'
+            
+              return;
+          }
+          
+          response = yield getSummary(vm.membershipType, vm.membershipId)
 
-            // Assign the character object of the response to a var
-            //var characterData = response.body.Response.data.characters;
-            vm.characterData = response.body.Response.data;
+          // Assign the character object of the response to a var
+          vm.characterData = response.data.Response.data;
 
-            let chars = vm.characterData.characters
+          let chars = vm.characterData.characters
 
-            /**
-             *  Iterate through that new object to obtain each characters base and store those seperately.
-             *  If your not familiar with the character API, the assignment below can seem confusing. Let me
-             *  explain -
-             *  Take note of the character object above. Each API response is wrapped in a Response object.
-             *  Inside that is a data object, followed by a characters array object, which stores all your
-             *  characters within another object called a "character base". It would looke something like this:
-             *  'Response.data.characters[0].characterBase' - Would be the first character you created.
-             *  'Response.data.characters[1].characterBase' - Would be the second character you created.
-             *  'Response.data.characters[2].characterBase' - Would be the third character you created.
-             *  So... I am iterating over the characters objects and storing each one within a characterBase
-             *  object in the data store.
-             */
-            for (let i = 0; i < chars.length; i++) {
-              vm.characterBase.push(chars[i]);
-            }
+          vm.characterBase = []; // reset the array - forces Vue to refresh
 
-            vm.activeCharacter = vm.characterBase.filter(this.isActive)
+          for (let i = 0; i < chars.length; i++) {
+            vm.characterBase.push(chars[i]);
+            if (chars[i].characterBase.currentActivityHash > 0) {
+              vm.activeCharacter = chars[i];
+            };
+          }
 
-            // Just for displaying that we successfully grabed an active character
-            if (vm.activeCharacter != '') {
-              console.log('Active Character: ' + vm.activeCharacter)
-            } else {
-              console.log('It seems the player with the ID of ' + vm.membershipId + ' doesn\'t have any active characters.')
-            }
-
-            if (vm.activeCharacter == 0) {
-              vm.message = 'None of your characters seem to be in an activity'
-            }
+          // Just for displaying that we successfully grabed an active character
+          if (vm.activeCharacter) {
+            console.log('Active Character: ' + vm.activeCharacter)
+          } else {
+            console.log('It seems the player with the ID of ' + vm.membershipId + ' doesn\'t have any active characters.')
+            vm.message = 'None of your characters seem to be in an activity'
+          }   
 
 
-          }, function (err) {
-              vm.message = 'Failed fetch your account summary: ' + err
-          })
-
-      },
+        } catch (err) {
+          console.error(err);
+          vm.message = 'Failed to assemble characters: ' + err.statusText;
+        }
+          
+      }),
 
       isActive: function(characters) {
         return characters.characterBase.currentActivityHash > 0;
