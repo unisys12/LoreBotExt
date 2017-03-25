2daf00e48aa18562f8ec798f4089e2aadd4a77ea
Phillip Jackson <unisys12@gmail.com>
2017-03-24 21:39:45 -0500

    implement character component

--- src/components/character.vue
+++ src/components/character.vue
@@ -2,11 +2,10 @@
 <section class="section">
   <div class="container">
     <span class="title">Active Character</span>
-    <span class="subtitle">{{ fetchGamer }}</span>
     <hr>
     <div class="columns">
       <div class="column">
-        <span>Character Emblem Stuff Here!</span>
+        <pre v-if="fetchGamer" :thisAccount="getActivity(fetchGamer)">{{  }}</pre>
       </div>
     </div>
   </div>
@@ -22,190 +21,250 @@ export default {
   name: 'Character',
   Store,
   data () {
-    return {}
+    return {
+      gamer: ''
+    }
   },
   computed: {
     fetchGamer: function() {
       return Store.getters.fetchGamer
     },
-    fetchCharacter: co.wrap(function *(){
-      if(fetchGamer){
-        console.log(fetchGamer)
-        return fetchGamer
-      }else{
-        console.log('Could not fetchGamer...')
-      }
-    })
+    fetchAccount: function() {
+      return Store.getters.fetchAccount
+    },
+    fetchSummary: function() {
+      return Store.getters.fetchSummary
+    },
+    fetchActiveCharacter: function() {
+      return Store.getters.fetchActive
+    }
   },
   methods: {
-    assembleCharacters: co.wrap(function *(){
-
-    })
+    getPlayer: co.wrap(function *(obj) {
+      let res = yield searchDestinyPlayer(obj)
+      let account = res.data.Response
+      if(account.length > 0){
+        Store.commit('storeAccount', account)
+      }else{
+        // Display message that the account could not be found
+        console.log('Something went wrong, cannot store the account: ', account)
+        return
+      }
+    }),
+    getAccountSummary: co.wrap(function *(obj) {
+      if(this.fetchGamer){
+        // Find the player on BNet and store their info
+        this.getPlayer(obj)
+        // Fetch that info from the Store and put into a var 
+        let account = this.fetchAccount
+        // Use map to extract the data we need for the next API call and 
+        // place it in the Store for further use... later...
+        account.map((x)=>{
+          Store.commit('storeMembershipId', x.membershipId)
+          Store.commit('storeMembershipType', x.membershipType)
+        })
+        let summary = yield getSummary(Store.state.membershipType, Store.state.membershipId)
+        if(summary.data.ErrorCode != 1){
+          console.log('Error fetching Summary: ', summary)
+          console.log(summary.data.ErrorStatus + ' :' + summary.data.Message)
+        }else{
+          Store.commit('storeSummary', summary)
+        }        
+      }else{
+        // Return message to display that no account was found
+        console.log('No gamertag entered or found...')
+        return
+      }
+    }),
+    getActiveCharacter: async function(obj) {
+      await this.getAccountSummary(obj)
+      let characters = this.fetchSummary
+      let chars = characters.data.Response.data.characters
+      chars.map((x)=>{
+        if(x.characterBase.currentActivityHash > 0) {
+          Store.commit('storeActiveCharacters', x)
+        }
+      })
+    },
+    getActivity: async function(obj) {
+      await this.getActiveCharacter(obj)
+      let activeCharacter = this.fetchActiveCharacter
+      if(activeCharacter.length < 1){
+        // Send message to display 
+        console.log('There are currently no active characters on ' + obj + "'s account...")
+        return
+      }else{
+        console.log('Found active character... fetching activity now...')
+        let activityHash = activeCharacter.characterBase.currentActivityHash
+        let activity = await getActivity(activityHash)
+        Store.commit('storeActivity', activity.data.Response.data.activity)
+      }
+    }
   }
 }
 </script>
 
 <style></style>
 
-'use strict'
+// 'use strict'
 
-import Vue from 'vue'
-import { searchDestinyPlayer, getIdByDisplayName, getSummary, getActivity, getClass, getGender, getRace } from '../Bungie/api.js'
-import Ishtar from '../Ishtar/api'
-import grimoireDefinitions from '../Bungie/grimoireDefs'
-let co = require('co')
+// import Vue from 'vue'
+// import { searchDestinyPlayer, getIdByDisplayName, getSummary, getActivity, getClass, getGender, getRace } from '../Bungie/api.js'
+// import Ishtar from '../Ishtar/api'
+// import grimoireDefinitions from '../Bungie/grimoireDefs'
+// let co = require('co')
 
 
-new Vue({
+// new Vue({
 
-    el: '#character',
+//     el: '#character',
 
-    data: {
-        message: 'Waiting for your characters to enter an activity...',
-        cardMessage: 'Sorry, there does not seem to be a Grimoire Card for this activity... BOO!',
-        categoryMessage: '',
-        displayName: '',
-        membershipType: '',
-        membershipId: '',
-        characterBase: [],
-        activeCharacter: '',
-        characterGender: '',
-        characterRace: '',
-        characterClass: '',
-        activity: '',
-        grimoireCard: '',
-        grimoireCategories: ''
-    },
+//     data: {
+//         message: 'Waiting for your characters to enter an activity...',
+//         cardMessage: 'Sorry, there does not seem to be a Grimoire Card for this activity... BOO!',
+//         categoryMessage: '',
+//         displayName: '',
+//         membershipType: '',
+//         membershipId: '',
+//         characterBase: [],
+//         activeCharacter: '',
+//         characterGender: '',
+//         characterRace: '',
+//         characterClass: '',
+//         activity: '',
+//         grimoireCard: '',
+//         grimoireCategories: ''
+//     },
 
-    methods: {
-        assembleCharacters: co.wrap(function *() {
+//     methods: {
+//         assembleCharacters: co.wrap(function *() {
           
-          let vm = this
+//           let vm = this
           
-          try {
+//           try {
           
-            let res = yield searchDestinyPlayer(vm.displayName)          
-            let player = res.data.Response
+//             let res = yield searchDestinyPlayer(vm.displayName)          
+//             let player = res.data.Response
             
-            if(player.length > 0){
-              player.map((x)=>{
-                vm.membershipId = x.membershipId
-                vm.membershipType = x.membershipType
-              })
-            }else{
-              vm.message = 'There does not seem to be a player in the world of Destiny with the name <i>' + vm.displayName + '</i>, on either console. Make sure you enter a vaild gamertag (xbl or psn) or Bungie username.'
-              return
-            } 
+//             if(player.length > 0){
+//               player.map((x)=>{
+//                 vm.membershipId = x.membershipId
+//                 vm.membershipType = x.membershipType
+//               })
+//             }else{
+//               vm.message = 'There does not seem to be a player in the world of Destiny with the name <i>' + vm.displayName + '</i>, on either console. Make sure you enter a vaild gamertag (xbl or psn) or Bungie username.'
+//               return
+//             } 
             
-            let response = yield getSummary(vm.membershipType, vm.membershipId)
+//             let response = yield getSummary(vm.membershipType, vm.membershipId)
 
-            if(response.data.ErrorCode != 1){
-              vm.message = response.data.ErrorStatus + ': ' + response.data.Message
-              return
-            }
+//             if(response.data.ErrorCode != 1){
+//               vm.message = response.data.ErrorStatus + ': ' + response.data.Message
+//               return
+//             }
 
-            vm.characterData = response.data.Response.data;
+//             vm.characterData = response.data.Response.data;
 
-            let chars = vm.characterData.characters
+//             let chars = vm.characterData.characters
 
-            /**
-             * TO-DO
-             * Extract the code below, to line 90 at least,
-             * to external file
-             */
-            let active = []
+//             /**
+//              * TO-DO
+//              * Extract the code below, to line 90 at least,
+//              * to external file
+//              */
+//             let active = []
 
-            // Find any active characters
-            for (let i = 0; i < chars.length; i++) {
-              if (chars[i].characterBase.currentActivityHash > 0) {
-                active.push(chars[i])
-              }
-            }
+//             // Find any active characters
+//             for (let i = 0; i < chars.length; i++) {
+//               if (chars[i].characterBase.currentActivityHash > 0) {
+//                 active.push(chars[i])
+//               }
+//             }
 
-            if(active.length > 0) {
-              vm.activeCharacter = active[0]
+//             if(active.length > 0) {
+//               vm.activeCharacter = active[0]
 
-              // Will extract the following three maps to a single method later
-              let characterRace = []
-              characterRace.push(yield getRace(vm.activeCharacter.characterBase.raceHash))
-              characterRace.map((x)=>{
-                vm.characterRace = x.data.Response.data.race.raceName
-              })
+//               // Will extract the following three maps to a single method later
+//               let characterRace = []
+//               characterRace.push(yield getRace(vm.activeCharacter.characterBase.raceHash))
+//               characterRace.map((x)=>{
+//                 vm.characterRace = x.data.Response.data.race.raceName
+//               })
 
-              let characterClass = []
-              characterClass.push(yield getClass(vm.activeCharacter.characterBase.classHash))
-              characterClass.map((x)=>{
-                vm.characterClass = x.data.Response.data.classDefinition.className
-              })
+//               let characterClass = []
+//               characterClass.push(yield getClass(vm.activeCharacter.characterBase.classHash))
+//               characterClass.map((x)=>{
+//                 vm.characterClass = x.data.Response.data.classDefinition.className
+//               })
 
-              let characterGender = []
-              characterGender.push(yield getGender(vm.activeCharacter.characterBase.genderHash))
-              characterGender.map((x)=>{
-                vm.characterGender = x.data.Response.data.gender.genderName
-              })    
+//               let characterGender = []
+//               characterGender.push(yield getGender(vm.activeCharacter.characterBase.genderHash))
+//               characterGender.map((x)=>{
+//                 vm.characterGender = x.data.Response.data.gender.genderName
+//               })    
 
-              //yield this.resolveActivity
-              let activity = vm.activeCharacter.characterBase.currentActivityHash
-              let act = yield getActivity(activity)
+//               //yield this.resolveActivity
+//               let activity = vm.activeCharacter.characterBase.currentActivityHash
+//               let act = yield getActivity(activity)
 
-              vm.activity = act.data.Response.data.activity
+//               vm.activity = act.data.Response.data.activity
 
-              yield this.fetchActivityCards(vm.activity.activityName)
+//               yield this.fetchActivityCards(vm.activity.activityName)
               
-            } else {
-              vm.message = 'None of your characters seem to be in an activity'
-          }
+//             } else {
+//               vm.message = 'None of your characters seem to be in an activity'
+//           }
 
-        } catch (err) {
-          console.error(err);
-          vm.message = 'Failed to assemble characters: ' + err;
-        }
+//         } catch (err) {
+//           console.error(err);
+//           vm.message = 'Failed to assemble characters: ' + err;
+//         }
           
-      }),
+//       }),
 
-      checkActivity: function() {
-        try {
-          console.log('Reload Method Fired!')
-          setInterval(this.assembleCharacters, 10000)
-        } catch (e) {
-          console.log('Error from checkActivity' + e)
-        }
-      },
+//       checkActivity: function() {
+//         try {
+//           console.log('Reload Method Fired!')
+//           setInterval(this.assembleCharacters, 10000)
+//         } catch (e) {
+//           console.log('Error from checkActivity' + e)
+//         }
+//       },
 
-      resolveActivity: co.wrap(function *() {
-        let vm = this
-        let activity = vm.activeCharacter.characterBase.currentActivityHash
-        let act = yield getActivity(activity)
+//       resolveActivity: co.wrap(function *() {
+//         let vm = this
+//         let activity = vm.activeCharacter.characterBase.currentActivityHash
+//         let act = yield getActivity(activity)
 
-        vm.activity = act.data.Response.data.activity
-      }),
+//         vm.activity = act.data.Response.data.activity
+//       }),
 
-      /**
-       * TO-DO 
-       * =====
-       * Need to better handle activities that do not have a Grimoire Card
-       * so we don't spam ishtar api with bad requests. What is below does
-       * properly display a message, but does not stop calling ishtar api.
-       */
-      fetchActivityCards: async function(string) {
+//       /**
+//        * TO-DO 
+//        * =====
+//        * Need to better handle activities that do not have a Grimoire Card
+//        * so we don't spam ishtar api with bad requests. What is below does
+//        * properly display a message, but does not stop calling ishtar api.
+//        */
+//       fetchActivityCards: async function(string) {
         
-        let vm = this
-        //let cards = yield Ishtar.getCards(this.processSlug(string))
-        if(vm.activity != '') {
-          let cards = await grimoireDefinitions.fetchCards(string)
+//         let vm = this
+//         //let cards = yield Ishtar.getCards(this.processSlug(string))
+//         if(vm.activity != '') {
+//           let cards = await grimoireDefinitions.fetchCards(string)
           
-          // Store grimoire card data
-          if(cards){
-            vm.grimoireCard = cards
-          } 
-        }             
+//           // Store grimoire card data
+//           if(cards){
+//             vm.grimoireCard = cards
+//           } 
+//         }             
       
-      }
+//       }
 
-    },
+//     },
 
-    computed: {
+//     computed: {
 
-    }
+//     }
 
-})
+// })
--- src/store.js
+++ src/store.js
@@ -5,16 +5,58 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state:{
-    displayName: ''
+    displayName: '',
+    account: '',
+    summary: '',
+    membershipId: '',
+    membershipType: '',
+    activeCharacters: [],
+    activity: ''
   },
   mutations: {
     storeDisplayName: function(state, string) {
       state.displayName = string
+    },
+    storeAccount: function(state, account) {
+      state.account = account
+    },
+    storeSummary: function(state, summary) {
+      state.summary = summary
+    },
+    storeMembershipId: function(state, membershipId) {
+      state.membershipId = membershipId
+    },
+    storeMembershipType: function(state, membershipType) {
+      state.membershipType = membershipType
+    },
+    storeActiveCharacters: function(state, character) {
+      state.activeCharacters = character
+    },
+    storeActivity: function(state, activity) {
+      state.activity = activity
     }
   },
   getters: {
     fetchGamer: function(state) {
       return state.displayName
+    },
+    fetchAccount: function(state) {
+      return state.account
+    },
+    fetchSummary: function(state) {
+      return state.summary
+    },
+    fetchMembershipId: function(state) {
+      return state.membershipId
+    },
+    fetchMembershipType: function(state) {
+      return state.membershipType
+    },
+    fetchActive: function(state) {
+      return state.activeCharacters
+    }, 
+    fetchActivity: function(state) {
+      return state.activity
     }
   }
 })
